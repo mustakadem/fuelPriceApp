261224bf5aff0dd5479915683e11dddfee68d967
Álvaro González <alvarogonzalez@z1.digital>
2021-03-17 19:02:52 +0100

    fix: add auth to main module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import depthLimit from 'graphql-depth-limit';
 import { ScalarsModule } from './scalars/scalars.module';
 import GraphQLJSON from 'graphql-type-json';
 import { ScheduleModule } from '@nestjs/schedule';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -26,6 +27,7 @@ import { ScheduleModule } from '@nestjs/schedule';
     }),
     ScalarsModule,
     ScheduleModule.forRoot(),
+    AuthModule,
   ],
 })
 export class AppModule {}
